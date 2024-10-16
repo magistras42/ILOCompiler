4668842f90dab5ada8c4a5cc9cdfd8071b669a6d
Maggie Simmons <90944643+magistras42@users.noreply.github.com>
2024-10-16 03:31:45 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,120 @@
+/**
+ * COMP 412 Lab 2 Driver Code
+ */
+
+#include "alloc.h"
+
+void print_help() {
+    printf("COMP Reference Allocator (Lab 2)\n");
+    printf("Command Syntax:\n");
+    printf("\t412alloc [k] [-x] filename [-h]\n");
+    printf("\n");
+    printf("Required Arguments:\n");
+    printf("\tk\t specifies the number of registers available to the allocator\n");
+    printf("\tfilename\tis the path (absolute or relative) to the input file\n");
+    printf("\n");
+    printf("Optional Flags:\n");
+    printf("\t-h\tprints this message\n");
+    printf("\t-x\trenames registers and prints ILOC IR to command line\n");
+}
+
+int main(int argc, char **argv) {
+    // Flags and their values
+    uint8_t hflag = 0;
+    uint8_t xflag = 0;
+    uint8_t flags = 0;
+
+    extern char *optarg;
+    extern int optind, optopt;
+    
+    int status = 0;
+    int arg;
+    ctx* context;
+    StateIR* ir;
+    char *filename;
+
+    while ((arg = getopt(argc, argv, ":hx")) != -1) {
+    switch(arg) {
+        case 'h':
+            hflag++;
+            flags++;
+            break;
+        case 'x':
+            xflag++;
+            flags++;
+            break;
+        case '?':
+            fprintf(stderr, "Unrecognized option: -%c\n", optopt);
+            print_help();
+            status = -1;
+            return status;
+        }
+    }
+
+    // could check if filename uninitialized, if not then do this?
+    filename = argv[argc - 1];
+
+    if (flags > 1) {
+        fprintf(stderr, "ERROR: Multiple command-line flags found.\nTry '-h' for information on command-line syntax.\n");
+    }
+
+    if ((strlen(filename) < 1) || (argc <= 1)) {
+        fprintf(stderr, "ERROR: Missing file path.\n");
+        print_help();
+        return status;
+    }
+
+    // Process Flags
+    if (hflag) {
+        print_help();
+        return(0);
+    }
+
+    // Check to open file
+    context = (ctx*) malloc(sizeof(ctx));
+    if (context == NULL) {
+        fprintf(stderr, "ERROR: Failed to initialize global context\n");
+        return -1;
+    }
+    if ((status = init_ctx(context, filename))) {
+        fprintf(stderr, "ERROR: Could not open input file.\n");
+        print_help();
+        return status;
+    }
+
+    // Initialize IR Representation
+    ir = (StateIR*) malloc(sizeof(StateIR));
+    if (ir == NULL) {
+        fprintf(stderr, "ERROR: Failed to initialize IR State\n");
+        fclose(context->file);
+        return -1;
+    }
+    if ((status = init_IR(ir))) {
+        fprintf(stderr, "ERROR: Failed to initialize IR State\n");
+        fclose(context->file);
+        return status;
+    }
+
+    context->ir = ir;
+
+    if (xflag) {
+        if ((status = parse(context)) >= 0) {
+            if ((status = rename_reg(context->ir)) >= 0) {
+                // printf("here!\n");
+                print_vr(context->ir);
+                fclose(context->file);
+                return 0;
+            }
+            printf("Failed to allocate intermediate arrays, run terminates.\n");
+        }
+        printf("Due to the syntax error, run terminates.\n");
+        fclose(context->file);
+        return status;
+    }
+
+    // TODO: code for lab2
+
+    // Close open file
+    fclose(context->file);
+    return 0;
+}
