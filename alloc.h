e30a5b993b2f009b6725c6840dd6b9c4b44c7468
Maggie Simmons <90944643+magistras42@users.noreply.github.com>
2024-10-21 00:02:55 -0500

    Update alloc.h

--- alloc.h
+++ alloc.h
@@ -14,6 +14,8 @@
 #ifndef ALLOC_H_INCLUDED
 #define ALLOC_H_INCLUDED
 
+#define INVALID 65
+
 /* STRUCTS */
 
 // TODO: instead of array, maybe have stack struct
@@ -25,13 +27,13 @@ typedef struct {
     uint32_t index;
     uint32_t max_pr;        // number of unreserved registers
     uint32_t spill_adr;
-    uint32_t pr_count;
-    uint32_t pr_stack_size;
-    uint32_t** pr_to_vr;
-    uint32_t** prnu;
-    uint32_t** pr_stack;
-    uint32_t** vr_to_pr;
-    uint32_t** vr_to_spill;
+    uint32_t pr_count;      // actual number of physical registers
+    uint32_t pr_stack_size;   
+    uint32_t* pr_to_vr;
+    uint32_t* prnu;
+    uint32_t* pr_stack;
+    uint32_t* vr_to_pr;
+    uint32_t* vr_to_spill;
     StateIR* ir;
     struct IRLine* cur;
 } Alloc_State;
@@ -44,13 +46,14 @@ int rename_reg(StateIR* ir);
 
 // Register Allocation Functions
 void reallocate(Alloc_State* ctx, uint32_t max_vr);
-uint32_t get_a_pr(Alloc_State* ctx, uint32_t vr, uint32_t nu);
+uint32_t get_a_pr(Alloc_State* ctx, uint32_t vr, uint32_t nu, uint32_t marked);
 void free_a_pr(Alloc_State* ctx, uint32_t pr);
 void spill(Alloc_State* ctx, uint32_t pr);
 void restore(Alloc_State* ctx, uint32_t vr, uint32_t nu);
 
 /* ILOC IR REPRESENTATION CODE */
 void print_vr(StateIR* ir);
+void print_pr(StateIR* ir);
 
 /* ALLOC_STATE MAINTENANCE CODE */
 int init_alloc(Alloc_State* ctx, StateIR* ir, uint32_t max_vr, uint32_t max_pr);
